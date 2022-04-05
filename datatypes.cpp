3e955cde6d7ee7814ee00284f0536ba0f5aa626b
KAMALESUWARAN-D <kamal.npp0874@gmail.com>
2022-04-05 21:26:49 +0530

    'add'

---
+++ datatypes.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    float f;
+    char c;
+    string str;
+    cout<<"Enter any integer";
+    cin>>a;
+    cout<<"Enter any float value";
+    cin>>f;
+    cout<<"Enter any character";
+    cin>>c;
+    cout<<"Enter any string";
+    cin>>str;
+    cout<<"You entered integer is "<<a;
+    cout<<"You entered float is "<<f;
+    cout<<"You entered character is "<<c;
+    cout<<"You entered string is "<<str;
+
+    return 0;
+
+}
